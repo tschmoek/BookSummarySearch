f9787b0e0d0cad407555700f3095ddd3f6920012
Tanner Schmoekel <tschmoek@gmail.com>
2017-06-06 19:59:46 -0500

    Changing static settings..

--- BookSummarySearch/settings.py
+++ BookSummarySearch/settings.py
@@ -125,8 +125,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/Search/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = ( 
-      'C:/Source/BookSummarySearch/Search/static/',  
+# STATIC_URL = '/Search/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = ( 
+#       'C:/Source/BookSummarySearch/Search/static/',  
+# )
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
 )
